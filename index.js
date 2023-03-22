52c3227f8fcde22b6873f68b36f66e1c3218c800
amacken3 <aengus.2000@gmail.com>
2023-03-22 17:31:21 -0400

    done with assignment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const name = "Susan";
 const height = 39;
 const message = `${name} is ${height} inches tall`;
-
+const hello = "hello";
 module.exports = {name, height, message};
 console.log(message)
